cfd7fa2c9cc186badb3083022d7e5749c72a6e2d
MasudaTaiga <masuda.taiga@laiblitz.co.jp>
2026-04-21 15:48:05 +0900

    Issue6 解消

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -26,8 +26,6 @@ class CalendarsController < ApplicationController
     @todays_date = Date.today
     # 例)　今日が2月1日の場合・・・ Date.today.day => 1日
 
-    @todays_wday = Date.today.wday
-
     @week_days = []
 
     plans = Plan.where(date: @todays_date..@todays_date + 6)
@@ -38,7 +36,7 @@ class CalendarsController < ApplicationController
         today_plans.push(plan.plan) if plan.date == @todays_date + x
       end
       
-      wday_num = @todays_wday[x]# wdayメソッドを用いて取得した数値
+      wday_num = Date.today.wday + x
       if wday_num >= 7 #「wday_numが7以上の場合」という条件式
         wday_num = wday_num -7
       end
